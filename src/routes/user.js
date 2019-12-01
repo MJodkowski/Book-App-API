8477ecebbb33ca75a48d8accad8710cf36a9270f
Mieczysław Jodkowski <mj@nebulic.com>
2019-12-02 00:49:08 +0100

    Add auth route for user and modify book review routes.

--- src/routes/user.js
+++ src/routes/user.js
@@ -37,15 +37,26 @@ router.post('/logoutall', auth, async (req, res) => {
 router.post('/register', async (req, res) => {
     try {
         if (await User.findOne({ name: req.body.name })) {
-            res.send('User already exists!');
+            return res.status(400).send('User already exists!');
         }
         user = new User(req.body);
+        const token = await user.generateToken();
+        res.cookie('authToken', token, { httpOnly: true });
         await user.save();
-        res.send(user);
+        res.send({user, token});
     } catch (err) {
         res.status(400).send(err);
     }
 });
+
+router.post('/authenticate',  auth, async (req, res) => {
+    try {
+        res.send({ user: req.user });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
 
 
 module.exports = router;
